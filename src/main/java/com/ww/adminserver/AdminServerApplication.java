da831a2126f8c9fa4a2a786954eea5d4a0392b7d
vivianwei808 <vivianwei808>
2021-09-13 15:06:55 +0800

    不需要

--- src/main/java/com/ww/adminserver/AdminServerApplication.java
+++ src/main/java/com/ww/adminserver/AdminServerApplication.java
@@ -10,9 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAdminServer
 public class AdminServerApplication {
 
-    @Autowired
-    private StringEncryptor encryptor;
-
     public static void main(String[] args) {
         SpringApplication.run( AdminServerApplication.class, args );
     }
