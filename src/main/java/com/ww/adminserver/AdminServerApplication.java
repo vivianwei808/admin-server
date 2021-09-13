973b6b209258abaf53a10ae8e21bde733f7eb2b8
vivianwei808 <vivianwei808>
2021-09-13 15:01:31 +0800

    Initial commit

---
+++ src/main/java/com/ww/adminserver/AdminServerApplication.java
@@ -0,0 +1,21 @@
+package com.ww.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    @Autowired
+    private StringEncryptor encryptor;
+
+    public static void main(String[] args) {
+        SpringApplication.run( AdminServerApplication.class, args );
+    }
+
+}
+
